8bedf74f3e13fd8f3f7d6342367fda194bf3d483
nicolasLuna1024 <nicolas.luna@epn.edu.ec>
2025-05-30 21:59:14 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'supabase-upload',
+  webDir: 'www'
+};
+
+export default config;
